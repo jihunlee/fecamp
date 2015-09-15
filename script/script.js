9e42342935c62c051e5c8104a311e345c0f77801
Ji hun Lee <jihunlee0523@Jis-MacBook-Pro-2.local>
2015-09-15 22:11:10 +0900

    no message

---
+++ script/script.js
@@ -0,0 +1,25 @@
+var numbers = [100, 200, 300];
+var nested = [[1, 2, 3], ['a', 'b', 'c']];
+    
+var 수강생 = {
+    이름: '이지훈',
+    성별: '남자',
+    개발자: true
+};
+    
+function 원소(arg1, arg2) {
+    return arg1[arg2];   
+}
+    
+    
+function 세제곱(num){
+    return Math.pow(num, 3);
+}
+    
+var today = new Date();
+
+function 날짜(date){
+    var day = '일월화수목금토';
+    return `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 ${day[date.getDay()]}요일`;
+}
+   
